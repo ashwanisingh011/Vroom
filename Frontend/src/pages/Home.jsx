4c8eee3d788d4da6c1a81005c2fe3cc4e870ccfd
ashwanisingh011 <ashwanisingh9737@gmail.com>
2025-11-04 23:32:29 +0530

    frontend stuff

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='h-screen pt-5 pl-5 flex justify-between flex-col w-full bg-red-400'>
+      <img className='h-10 ' src=""/>
+        <div className='bg-white'>
+          <h1>Welcome to Vroom!</h1>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
